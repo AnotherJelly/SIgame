621dd87f09fc24128643a52b6a28b98c1ff86c12
AnotherJelly <bespalov-maksim@list.ru>
2025-06-25 14:46:56 +0300

    Add example data

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,7 +10,10 @@ export function App() {
 
     const [playersData, dispatchPlayers] = useReducer(playersReducer, initialPlayers);
 
-    const [rounds, setRounds] = useState([]);
+    // Example Data
+    const exampleData = [{"_id":"682f6315277a2d316aa0df13","id":"itkl1oxx8","name":"Round 1","categories":[{"id":"2wehoyetm","name":"Тема 1","questions":[{"id":"xiniwczrq","questionType":"bet","text":"Вопрос 1","answer":"Ответ 1","_id":"682f6315277a2d316aa0df15"},{"id":"fx4b968oj","questionType":"ordinary","text":"Вопрос 2","answer":"Ответ 2","_id":"682f6315277a2d316aa0df16"},{"id":"v9rotiale","questionType":"ordinary","text":"Вопрос 3","answer":"Ответ 3","_id":"682f6315277a2d316aa0df17"},{"id":"ktmje9gkw","questionType":"ordinary","text":"Вопрос 4","answer":"Ответ 4","_id":"682f6315277a2d316aa0df18"},{"id":"cpj1e8mzz","questionType":"cat","text":"Вопрос 5","answer":"Ответ 5","_id":"682f6315277a2d316aa0df19"}],"_id":"682f6315277a2d316aa0df14"}],"__v":0},{"_id":"682f66aa277a2d316aa0df4f","id":"a5hpm7c0o","name":"Round 2","categories":[{"id":"0dy66zpyj","name":"Тема 2","questions":[{"id":"12jut8uxv","questionType":"ordinary","text":"Вопрос 6","answer":"Ответ 6","_id":"682f66aa277a2d316aa0df51"},{"id":"3vd3qie4w","questionType":"ordinary","text":"Вопрос 7","answer":"Ответ 7","_id":"682f66aa277a2d316aa0df52"},{"id":"h9vjhso3m","questionType":"ordinary","text":"Вопрос 8","answer":"Ответ 8","_id":"682f66aa277a2d316aa0df53"},{"id":"7hnomrga4","questionType":"ordinary","text":"Вопрос 9","answer":"Ответ 9","_id":"682f66aa277a2d316aa0df54"},{"id":"dz5px03kh","questionType":"ordinary","text":"Вопрос 10","answer":"Ответ 10","_id":"682f66aa277a2d316aa0df55"}],"_id":"682f66aa277a2d316aa0df50"}],"__v":0}]
+    const [rounds, setRounds] = useState(exampleData);
+    //const [rounds, setRounds] = useState([]);
 
     useEffect(() => {
         async function loadRounds() {
